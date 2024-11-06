b2fa7211b608f46d706eb15eb859cf7a643c9c3e
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-11-06 13:42:57 +0600

    chore: layout navbar

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -1,9 +1,10 @@
 import { Navbar } from "@/src/widgets/navbar";
 import { Home as HomeWidget } from "@/src/widgets/home";
+import styles from "../styles/styles.module.scss";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.page}>
       <HomeWidget />
       <Navbar />
     </main>
--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import styles from "../styles/styles.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 
 export const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <Link href="/lk" className={styles.link} style={{ opacity: 1 }}>
+        <Image
+          src="/icons/home.svg"
+          width={24}
+          height={24}
+          alt="Главная страница"
+        />
+        <span>Главная</span>
+      </Link>
+      <Link href="/lk" className={styles.request}>
+        <div className={styles.link}>
+          <Image src="/icons/plus.svg" width={24} height={24} alt="Заявка" />
+          <span>Заявка</span>
+        </div>
+      </Link>
+      <Link href="/profile" className={styles.link} style={{ opacity: 0.5 }}>
+        {" "}
+        <Image src="/icons/profile.svg" width={24} height={24} alt="Профиль" />
+        <span>Профиль</span>
+      </Link>
+    </div>
+  );
 };
