1726369a0f1639ef0be75a67de9d5a8e9e4e2640
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-11-06 09:01:17 +0600

    chore: create home widget & navbar

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -1,7 +1,11 @@
+import { Navbar } from "@/src/widgets/navbar";
+import { Home as HomeWidget } from "@/src/widgets/home";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-4 flex-col gap-4">
-      <h1 className="text-4xl font-bold">Create from here</h1>
+    <main>
+      <HomeWidget />
+      <Navbar />
     </main>
   );
 }
---
+++ src/widgets/home/ui/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Home: React.FC = () => {
+  return (
+    <div>
+      {" "}
+      <h1>Привет, Даша</h1>
+      <p>
+        То, что не убивает нас – делает нас сильнее –испытай себя уже сегодня!
+      </p>
+    </div>
+  );
+};
