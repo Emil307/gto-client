745346c3f539fb947c010c9b0944fe0b26e393d3
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-11-08 22:18:35 +0600

    chore: layout privacy

--- src/features/auth/ui/PrivacyStep/PrivacyStep.tsx
+++ src/features/auth/ui/PrivacyStep/PrivacyStep.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import authState from "@/src/entities/auth/store/authState";
+import { ParallelogramButton } from "@/src/shared/ui/parallelogramButton";
+import styles from "../../styles/styles.module.scss";
+import { Checkbox } from "@mantine/core";
 
 export const PrivacyStep: React.FC = () => {
   const router = useRouter();
+  const [firstChecked, setFirstChecked] = useState<boolean>(true);
+  const [secondChecked, setSecondChecked] = useState<boolean>(true);
 
   function handleConfirm() {
+    if (!firstChecked || !secondChecked) {
+      return;
+    }
     if (authState.registerDto) {
       authState.registration(authState.registerDto).then(() => {
         router.replace(
@@ -18,8 +26,31 @@ export const PrivacyStep: React.FC = () => {
   }
 
   return (
-    <div>
-      <button onClick={handleConfirm}>Продолжить</button>
-    </div>
+    <form className={styles.form}>
+      <div className={styles.checkboxes}>
+        <Checkbox
+          checked={firstChecked}
+          onChange={(event) => setFirstChecked(event.currentTarget.checked)}
+          label="Подтверждаю, что предоставленные мной сведения являются достоверными"
+        />
+        <Checkbox
+          checked={secondChecked}
+          onChange={(event) => setSecondChecked(event.currentTarget.checked)}
+          label="Даю согласие на сбор и обработку персональных данных*"
+        />
+      </div>
+      <div className={styles.privacyBottom}>
+        <ParallelogramButton onClick={handleConfirm}>
+          Продолжить
+        </ParallelogramButton>
+        <div>
+          <p className={styles.credits}>
+            Нажимая «Продолжить», вы соглашаетесь с условиями
+            <br />
+            <a>Пользовательского соглашения</a>
+          </p>
+        </div>
+      </div>
+    </form>
   );
 };
