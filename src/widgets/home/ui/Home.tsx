cbb1478b07f1930267c7ca4a17cef1ca5fef8230
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-11-11 03:10:15 +0600

    [feat] rating trigger button

--- src/widgets/home/ui/Home.tsx
+++ src/widgets/home/ui/Home.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import styles from "../styles/styles.module.scss";
 import { Event } from "@/src/features/events";
+import { RatingTrigger } from "@/src/features/rating";
 
 export const Home: React.FC = () => {
   return (
-    <div>
-      <div className={styles.offer}>
-        <h1 className={styles.title}>
-          Привет, <span>Даша</span>
-        </h1>
-        <p className={styles.quote}>
-          То, что не убивает нас – делает нас сильнее – испытай себя уже
-          сегодня!
-        </p>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.offer}>
+          <h1 className={styles.title}>
+            Привет, <span>Даша</span>
+          </h1>
+          <p className={styles.quote}>
+            То, что не убивает нас – делает нас сильнее – испытай себя уже
+            сегодня!
+          </p>
+        </div>
+        <div className={styles.articles}>
+          <RatingTrigger />
+          <Event />
+        </div>
       </div>
-      <Event />
     </div>
   );
 };
