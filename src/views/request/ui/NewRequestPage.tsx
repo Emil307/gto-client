abf22025606256e809c5d7c72bcefe938a62c6cf
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-11-18 23:09:25 +0600

    chore: requset title, fix: api

--- src/views/request/ui/NewRequestPage.tsx
+++ src/views/request/ui/NewRequestPage.tsx
@@ -4,6 +4,7 @@ import { RequestTabs } from "@/src/widgets/request";
 export default function Page() {
   return (
     <main className={styles.page}>
+      <h1 className={styles.title}>Новая заявка</h1>
       <RequestTabs />
     </main>
   );
