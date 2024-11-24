44e7539a97e33e28572ab85983891facf7cb71e6
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-11-24 17:56:50 +0600

    chore: home page user state

--- src/widgets/home/ui/Home.tsx
+++ src/widgets/home/ui/Home.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "../styles/styles.module.scss";
 import { Event } from "@/src/features/events";
 import { RatingTrigger } from "@/src/features/rating";
+import { observer } from "mobx-react-lite";
+import userState, { IUser } from "@/src/entities/user";
+import { getMe } from "@/src/entities/profile";
 
-export const Home: React.FC = () => {
+export const Home: React.FC = observer(() => {
+  const [user, setUser] = useState<IUser | null>(null);
+
+  useEffect(() => {
+    if (userState.user) {
+      setUser(userState.user);
+      return;
+    }
+
+    getMe()
+      .then((res) => {
+        userState.setUser(res.data);
+        setUser(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.offer}>
           <h1 className={styles.title}>
-            Привет, <span>Даша</span>
+            Привет
+            {user?.name ? (
+              <>
+                , <span>{user.name}</span>
+              </>
+            ) : (
+              <>!</>
+            )}
           </h1>
           <p className={styles.quote}>
             То, что не убивает нас – делает нас сильнее – испытай себя уже
@@ -23,4 +53,4 @@ export const Home: React.FC = () => {
       </div>
     </div>
   );
-};
+});
