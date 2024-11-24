340315b0dbe177882c06d0a98c2fcd04d04fac59
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-11-25 00:55:24 +0600

    chore: layout video tab

--- src/features/request/ui/VideoTab/VideoTab.tsx
+++ src/features/request/ui/VideoTab/VideoTab.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { ParallelogramButton } from "@/src/shared/ui/parallelogramButton";
 import React from "react";
+import styles from "../../styles/styles.module.scss";
+import { useRouter } from "next/navigation";
 
 export const VideoTab = () => {
-  return <div>VideoTab</div>;
+  const router = useRouter();
+
+  return (
+    <div className={styles.videoTab}>
+      <div className={styles.videoTabSquare}></div>
+      <div className={styles.videoTabBottom}>
+        <ParallelogramButton
+          onClick={() => router.replace("/request/record-video")}
+        >
+          Снять видео
+        </ParallelogramButton>
+        <p className={styles.credits}>
+          Нажимая на кнопку «Далее», вы подтверждаете, что заполненные вами
+          данные соответствуют действительности и введены корректно. Вы случае
+          предоставления ложной информации ваша заявка будет аннулирована.
+        </p>
+      </div>
+    </div>
+  );
 };
