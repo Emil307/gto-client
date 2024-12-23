dfddcda2f95d33bc15d491b41eeb538ec4eccc22
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-12-23 13:08:58 +0600

    feat: get rating

--- src/entities/rating/ui/list/RatingList.tsx
+++ src/entities/rating/ui/list/RatingList.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import ratingState from "../../store/ratingState";
 import { observer } from "mobx-react-lite";
 
 export const RatingList: React.FC = observer(() => {
+  useEffect(() => {
+    if (ratingState.rating.length === 0) {
+      ratingState.getRating({
+        filters: {},
+      });
+    }
+  }, []);
+
   return (
     <div>
       {ratingState.rating.map((rating) => (
