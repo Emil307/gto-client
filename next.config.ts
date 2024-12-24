fe4f6668baf629d3747210cb888caa13253faf8a
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-12-24 10:38:54 +0600

    fix: build errors

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-import NextBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-const withBundleAnalyzer = NextBundleAnalyzer({
-  enabled: true,
-});
-
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
