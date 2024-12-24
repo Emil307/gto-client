b91030ea17c8503ea93f7826144b812682dbdc26
EMILNOVIKOV <emilnovikov86@gmail.com>
2024-12-24 10:25:26 +0600

    deps: bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import NextBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withBundleAnalyzer = NextBundleAnalyzer({
+  enabled: true,
+});
+
+export default withBundleAnalyzer(nextConfig);
