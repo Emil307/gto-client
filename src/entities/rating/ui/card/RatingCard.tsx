fe57194a13ee968c800135ad647a5cdf7694375c
EMILNOVIKOV <emilnovikov86@gmail.com>
2025-01-04 21:23:16 +0300

    chore: rank icons

--- src/entities/rating/ui/card/RatingCard.tsx
+++ src/entities/rating/ui/card/RatingCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import { IRating } from "../../types";
+import Image from "next/image";
 
 interface IRatingCardProps {
   rating: IRating;
@@ -10,7 +11,15 @@ interface IRatingCardProps {
 export const RatingCard: React.FC<IRatingCardProps> = ({ rating, isMy }) => {
   return (
     <div className={styles.container}>
-      <h5 className={styles.rank}>{rating.rank}.</h5>
+      {rating.rank === "1" ? (
+        <Image src="/icons/1.svg" width={32} height={32} alt="1" />
+      ) : rating.rank === "2" ? (
+        <Image src="/icons/2.svg" width={32} height={32} alt="2" />
+      ) : rating.rank === "3" ? (
+        <Image src="/icons/3.svg" width={32} height={32} alt="3" />
+      ) : (
+        <h5 className={styles.rank}>{rating.rank}.</h5>
+      )}
       <div className={styles.info}>
         <div className={styles.nameWrapper}>
           {isMy && <span>(Вы)</span>}
