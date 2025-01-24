b3378e7dc7697fd97e46cbe23a0c953a8e132700
EMILNOVIKOV <emilnovikov86@gmail.com>
2025-01-24 17:05:41 +0600

    design video tab

--- src/views/request/ui/NewRequestPage.tsx
+++ src/views/request/ui/NewRequestPage.tsx
@@ -4,7 +4,6 @@ import { RequestTabs } from "@/src/widgets/request";
 export default function Page() {
   return (
     <main className={styles.page}>
-      <h1 className={styles.title}>Новая заявка</h1>
       <RequestTabs />
     </main>
   );
