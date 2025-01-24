383a175ca0d417c15e69317d959c26ae4e34e2a9
EMILNOVIKOV <emilnovikov86@gmail.com>
2025-01-24 17:49:25 +0600

    fix: send request

---
+++ src/entities/request/api/types.ts
@@ -0,0 +1,10 @@
+export interface RequestDTO {
+  surname: string;
+  name: string;
+  patronymic: string;
+  birthDate: string;
+  email: string;
+  region: string;
+  category_id: string;
+  phone: string;
+}
