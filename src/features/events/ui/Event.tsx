1d32509d0ecfd6fb355265ae1203193a4f435245
EMILNOVIKOV <emilnovikov86@gmail.com>
2025-01-31 23:16:09 +0600

    chore: change event image

--- src/features/events/ui/Event.tsx
+++ src/features/events/ui/Event.tsx
@@ -6,23 +6,26 @@ import { Button } from "@/src/shared";
 export const Event: React.FC = () => {
   return (
     <div className={styles.container}>
-      <Image
-        priority={true}
-        src="/icons/event-logo.svg"
-        width={100}
-        height={24}
-        alt="event logo"
-      />
-      <div className={styles.wrapper}>
-        <div className={styles.info}>
-          <div className={styles.dates}>26 октября – 2 ноября</div>
-          <h3 className={styles.title}>
-            Тренировочные сборы федерации многоборья ГТО России
-          </h3>
-        </div>
-        <div className={styles.buttons}>
-          <Button>Положение</Button>
-          <Button>Категории</Button>
+      <div className={styles.overlay}></div>
+      <div className={styles.content}>
+        <Image
+          priority={true}
+          src="/icons/event-logo.svg"
+          width={100}
+          height={24}
+          alt="event logo"
+        />
+        <div className={styles.wrapper}>
+          <div className={styles.info}>
+            <div className={styles.dates}>26 октября – 2 ноября</div>
+            <h3 className={styles.title}>
+              Тренировочные сборы федерации многоборья ГТО России
+            </h3>
+          </div>
+          <div className={styles.buttons}>
+            <Button>Положение</Button>
+            <Button>Категории</Button>
+          </div>
         </div>
       </div>
     </div>
