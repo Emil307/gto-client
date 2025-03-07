03db30fa396ad96076644a970e922c96cf7e00d2
EMILNOVIKOV <emilnovikov86@gmail.com>
2025-03-07 16:42:31 +0600

    fix: build errors

--- src/widgets/pdfViewer/PDFViewer.tsx
+++ src/widgets/pdfViewer/PDFViewer.tsx
@@ -6,25 +6,29 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
-interface IPdfViewerProps {
-  url: string;
-  origin: string;
-}
-
-export const PdfViewer: React.FC<IPdfViewerProps> = ({ url, origin }) => {
+export const PdfViewer: React.FC = () => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const searchParams = useSearchParams();
+
+  const url = searchParams.get("url");
+  const origin = searchParams.get("origin");
 
   return (
     <div className="h-screen w-screen bg-white">
-      <div>
-        <Link href={origin} className="p-2">
-          Назад
-        </Link>
-      </div>
+      {origin && (
+        <div>
+          <Link href={origin} className="p-2">
+            Назад
+          </Link>
+        </div>
+      )}
       <div className="flex-1">
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.10.111/build/pdf.worker.min.js">
-          <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
+          {url && (
+            <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
+          )}
         </Worker>
       </div>
     </div>
